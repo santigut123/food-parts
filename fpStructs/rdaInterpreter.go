dbdc454478c91922e34dce7152205597cc022e94
Santiago Gutierrez <santigut@posteo.net>
2021-11-03 19:12:01 -0400

    worked on RDA interpreter

---
+++ fpStructs/rdaInterpreter.go
@@ -0,0 +1,20 @@
+package fpStructs
+
+import (
+)
+
+type RDAInterpreter struct{
+	standard RDA
+	actual RDA
+}
+func MakeRDAInterpreter(actual RDA, standard RDA) *RDAInterpreter {
+	newRDAI := RDAInterpreter{
+		standard: standard,
+		actual:   actual,
+	}
+	return &newRDAI
+}
+func(RDAI *RDAInterpreter) GetNutriPercentages() map[string]int{
+
+
+}
